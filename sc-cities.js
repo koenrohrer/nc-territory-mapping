a248b03898c105cb2e57941f239a1882d71db101
koenrohrer <koenrohrer@att.net>
2026-01-20 06:03:44 -0500

    Add multi-state support with South Carolina map
    - Add tab navigation to switch between NC and SC maps
    - Add SC county boundaries (46 counties) and major cities data
    - Implement state-specific localStorage for separate assignments per state
    - Update projection settings dynamically based on selected state
    - Add .gitignore for development files

---
+++ sc-cities.js
@@ -0,0 +1,69 @@
+// South Carolina Major Cities (Population 50,000+)
+// Source: US Census Bureau 2020 estimates
+// Coordinates: [longitude, latitude] (WGS84)
+
+const scCitiesGeoJSON = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "properties": { "name": "Charleston", "population": 150227 },
+            "geometry": { "type": "Point", "coordinates": [-79.9311, 32.7765] }
+        },
+        {
+            "type": "Feature",
+            "properties": { "name": "Columbia", "population": 136632 },
+            "geometry": { "type": "Point", "coordinates": [-81.0348, 34.0007] }
+        },
+        {
+            "type": "Feature",
+            "properties": { "name": "North Charleston", "population": 114852 },
+            "geometry": { "type": "Point", "coordinates": [-79.9748, 32.8546] }
+        },
+        {
+            "type": "Feature",
+            "properties": { "name": "Mount Pleasant", "population": 89410 },
+            "geometry": { "type": "Point", "coordinates": [-79.8626, 32.8323] }
+        },
+        {
+            "type": "Feature",
+            "properties": { "name": "Rock Hill", "population": 74410 },
+            "geometry": { "type": "Point", "coordinates": [-81.0251, 34.9249] }
+        },
+        {
+            "type": "Feature",
+            "properties": { "name": "Greenville", "population": 70635 },
+            "geometry": { "type": "Point", "coordinates": [-82.3940, 34.8526] }
+        },
+        {
+            "type": "Feature",
+            "properties": { "name": "Summerville", "population": 53643 },
+            "geometry": { "type": "Point", "coordinates": [-80.1756, 33.0185] }
+        },
+        {
+            "type": "Feature",
+            "properties": { "name": "Goose Creek", "population": 47060 },
+            "geometry": { "type": "Point", "coordinates": [-80.0326, 32.9810] }
+        },
+        {
+            "type": "Feature",
+            "properties": { "name": "Hilton Head Island", "population": 44514 },
+            "geometry": { "type": "Point", "coordinates": [-80.7526, 32.2163] }
+        },
+        {
+            "type": "Feature",
+            "properties": { "name": "Florence", "population": 40596 },
+            "geometry": { "type": "Point", "coordinates": [-79.7626, 34.1954] }
+        },
+        {
+            "type": "Feature",
+            "properties": { "name": "Spartanburg", "population": 39363 },
+            "geometry": { "type": "Point", "coordinates": [-81.9320, 34.9496] }
+        },
+        {
+            "type": "Feature",
+            "properties": { "name": "Myrtle Beach", "population": 35682 },
+            "geometry": { "type": "Point", "coordinates": [-78.8867, 33.6891] }
+        }
+    ]
+};
